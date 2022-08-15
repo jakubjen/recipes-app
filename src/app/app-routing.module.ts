7abc761a6584f3c386bfb4cba93fb502b804f3a3
Jakub Jen <poczta.jakub.jen@gmail.com>
2022-08-15 11:07:30 +0200

    Add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component';
+import { RecipesEditComponent } from './recipes/recipes-edit/recipes-edit.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { NotFoundPageComponent } from './shared/not-found-page/not-found-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+	{ path: '', component: HomePageComponent },
+	{
+		path: 'recipes',
+		children: [
+			{ path: 'edit', component: RecipesEditComponent },
+			{ path: '', component: RecipesComponent },
+		],
+	},
+	{ path: '**', component: NotFoundPageComponent },
+];
 
 @NgModule({
 	imports: [RouterModule.forRoot(routes)],
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NotFoundPageComponent } from './not-found-page/not-found-page.component';
 
 @NgModule({
-	declarations: [],
+	declarations: [NotFoundPageComponent],
 	imports: [CommonModule],
+	exports: [NotFoundPageComponent],
 })
 export class SharedModule {}
