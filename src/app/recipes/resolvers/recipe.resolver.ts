893209aa60c9772a10d3939f12625ec9ce489f12
Jakub Jen <poczta.jakub.jen@gmail.com>
2022-08-19 12:32:54 +0200

    Load recipes only once

--- src/app/recipes/resolvers/recipe.resolver.ts
+++ src/app/recipes/resolvers/recipe.resolver.ts
@@ -2,18 +2,20 @@ import { Injectable } from '@angular/core';
 import { Resolve } from '@angular/router';
 import { Store } from '@ngrx/store';
 import RecipesActions from '@store/recipes/recipes.actions';
+import RecipesSelectors from '@store/recipes/recipes.selector';
+import { AppState } from '@store/store';
 import { finalize, first, Observable, tap } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class RecipeResolver implements Resolve<any> {
 	loading = false;
 
-	constructor(private store: Store) {}
+	constructor(private store: Store<AppState>) {}
 
 	resolve(): Observable<any> {
-		return this.store.pipe(
-			tap(() => {
-				if (!this.loading) {
+		return this.store.select(RecipesSelectors.selectLoadingIsComplied).pipe(
+			tap(recipesLoaded => {
+				if (!this.loading && !recipesLoaded) {
 					this.loading = true;
 					this.store.dispatch(RecipesActions.loadRecipes());
 				}
