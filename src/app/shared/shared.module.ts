422df043a73dc27ab26ae5949c32eae7404d37ab
Jakub Jen <poczta.jakub.jen@gmail.com>
2022-08-26 09:30:45 +0200

    Add translations

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NotFoundPageComponent } from './not-found-page/not-found-page.component';
 import { RouterModule } from '@angular/router';
+import { TranslateModule } from '@ngx-translate/core';
 
 @NgModule({
 	declarations: [NotFoundPageComponent],
-	imports: [CommonModule, RouterModule],
+	imports: [CommonModule, RouterModule, TranslateModule],
 	exports: [NotFoundPageComponent],
 })
 export class SharedModule {}
