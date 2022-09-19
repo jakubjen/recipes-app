29fcec728958f77d037abef1ef0fa74cfa5f3992
Jakub Jen <poczta.jakub.jen@gmail.com>
2022-09-19 14:40:30 +0200

    Add unit test to recipe filter pipe

--- src/app/recipes/filter-recipes.pipe.spec.ts
+++ src/app/recipes/filter-recipes.pipe.spec.ts
@@ -1,8 +1,32 @@
+import { mockRecipes } from '@models/recipe.mock.mode';
+import Recipe from '@models/recipe.model';
 import { FilterRecipesPipe } from './filter-recipes.pipe';
 
 describe('FilterRecipesPipe', () => {
-  it('create an instance', () => {
-    const pipe = new FilterRecipesPipe();
-    expect(pipe).toBeTruthy();
-  });
+	const recipes: Recipe[] = [mockRecipes.recipeOne, mockRecipes.recipeTwo];
+	const pipe = new FilterRecipesPipe();
+
+	it('create an instance', () => {
+		expect(pipe).toBeTruthy();
+	});
+
+	it('should filter by title', () => {
+		expect(pipe.transform(recipes, mockRecipes.recipeTwo.title)).not.toEqual([
+			mockRecipes.recipeOne,
+		]);
+
+		expect(pipe.transform(recipes, mockRecipes.recipeTwo.title)).toEqual([
+			mockRecipes.recipeTwo,
+		]);
+	});
+
+	it('should filter by descriptions', () => {
+		expect(
+			pipe.transform(recipes, mockRecipes.recipeTwo.description)
+		).not.toEqual([mockRecipes.recipeOne]);
+
+		expect(pipe.transform(recipes, mockRecipes.recipeTwo.description)).toEqual([
+			mockRecipes.recipeTwo,
+		]);
+	});
 });
