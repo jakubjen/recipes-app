e90592ba59272e0c8ba0a560283124a35583283b
Jakub Jen <poczta.jakub.jen@gmail.com>
2022-09-23 14:45:46 +0200

    Add guards

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -1,18 +1,32 @@
 import { NgModule } from '@angular/core';
+import {
+	AngularFireAuthGuard,
+	redirectLoggedInTo,
+} from '@angular/fire/compat/auth-guard';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+
+const redirectLoggedInToHome = () => redirectLoggedInTo(['/']);
 
 const routes: Routes = [
 	{
 		path: 'login',
 		component: LoginComponent,
-		data: { title: 'Login' },
+		canActivate: [AngularFireAuthGuard],
+		data: {
+			authGuardPipe: redirectLoggedInToHome,
+			title: 'Login',
+		},
 	},
 	{
 		path: 'register',
+		canActivate: [AngularFireAuthGuard],
 		component: RegisterComponent,
-		data: { title: 'Register' },
+		data: {
+			authGuardPipe: redirectLoggedInToHome,
+			title: 'Register',
+		},
 	},
 ];
 
