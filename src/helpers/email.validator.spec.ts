e8d692715232927b9de5a83d95434ada80534499
Jakub Jen <poczta.jakub.jen@gmail.com>
2022-09-26 13:22:42 +0200

    Add unit test for unit validator

---
+++ src/helpers/email.validator.spec.ts
@@ -0,0 +1,28 @@
+import { FormControl } from '@angular/forms';
+import { customEmailValidator } from './email.validator';
+
+const validator = customEmailValidator();
+
+const validData = [
+	'kuba@kuba.com',
+	'fbuef@id.pl',
+	'fbuef@id.eu',
+	'edu.edu.@edu.edu',
+];
+
+const incorrect = ['kuba@kuba', 'fbuefid.pl', 'fbuef@id.eu&^'];
+
+describe('customEmailValidator', () => {
+	it('should return null when adres is correct', () => {
+		validData.forEach(email => {
+			const formControl = new FormControl(email);
+			expect(validator(formControl)).toBeNull();
+		});
+	});
+	it('should return error when adres is correct', () => {
+		incorrect.forEach(email => {
+			const formControl = new FormControl(email);
+			expect(validator(formControl)).toEqual({ email: true });
+		});
+	});
+});
