2413d835b4901ed4b8aab9a585c5cb34c15adbb9
Jakub Jen <poczta.jakub.jen@gmail.com>
2022-10-20 09:20:28 +0200

    bug fix

--- src/helpers/is-nan.ts
+++ src/helpers/is-nan.ts
@@ -4,6 +4,6 @@ export const isNotANumber = () => {
 	return (control: AbstractControl): ValidationErrors | null => {
 		const number: string = control.value;
 		const regExp = new RegExp(/^\d*([,\.]\d+)?$/);
-		return regExp.test(number) ? null : { isNaN: true };
+		return regExp.test(number) || number === null ? null : { isNaN: true };
 	};
 };
